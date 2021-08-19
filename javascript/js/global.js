a9fd1a2e56aabd472bc8eb44b77f379700e021ea
najuzilu <junaluzi@gmail.com>
2021-08-19 13:07:27 +0200

    :sparkles: [NEW] declare global vars

---
+++ javascript/js/global.js
@@ -0,0 +1,6 @@
+var colors = [
+	"#466e12", "#77bc1f", "#C5E0B4",
+	"#FFD166", "#A9DDEB", "#009bde",
+	"#002060"
+].reverse();
+// var colors = ['#FFD166','#C6DABF','rgb(125, 207, 182)','rgb(0, 178, 202)','rgb(29, 78, 137)'];
